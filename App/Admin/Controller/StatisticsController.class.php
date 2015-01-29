b4b9682c77af04930550c0c141becd924278e125
DanielXLee <lixin8611@gmail.com>
2015-01-29 23:14:52 +0800

    update SQL

--- App/Admin/Controller/StatisticsController.class.php
+++ App/Admin/Controller/StatisticsController.class.php
@@ -18,41 +18,38 @@ class StatisticsController extends CommonController {
             $where = array();
             foreach ($search as $k=>$v) {
                 if(!$v) continue;
-                switch ($v){
-                    case 'day':
-                    case 'week':
-                    case 'month':
-                    case 'year': 
-                        $where = $k . " between " . self::getBegin($v) . " and " . self::getEnd($v); 
-                        break;
-                    default:
-                        if ($v[0] != '' && $v[1] == ''){
-                            $where = $k . " >= " . strtotime($v[0]);
-                        } else if($v[0] == '' && $v[1] != ''){
-                            $where = $k . " <= " . strtotime($v[1]);
-                        } else if($v[0] != '' && $v[1] != '') {
-                            $where = $k . " between " . strtotime($v[0]) . " and " . strtotime($v[1]);
-                        }
-                        break;
+                if ($v[0] != '' && $v[1] == ''){
+                    $where = $k . " >= " . strtotime($v[0]);
+                } else if($v[0] == '' && $v[1] != ''){
+                    $where = $k . " <= " . strtotime($v[1]);
+                } else if($v[0] != '' && $v[1] != '') {
+                    $where = $k . " between " . strtotime($v[0]) . " and " . strtotime($v[1]);
                 }
             } 
             // $where = implode(' and ', $where);
-            trace($where,'Where: ');
+            trace($where,'Where ');
             $limit=($page - 1) * $rows . "," . $rows;
             $total = $cost_db->where($where)->count();
             $order = "id ".$order;
             $field= array('pid','real_pay','FROM_UNIXTIME(modified, "%Y-%m-%d %H:%i:%s") as modified','operate_id','point');
             $list = $total ? $cost_db->field($field)->where($where)->order($order)->limit($limit)->select() : array();
-            trace($list, 'List: ');
-            $data = array('total'=>$total, 'rows'=>$list);
+            $totalPay = 0;
+            foreach ($list as $value) {
+                $totalPay += $value['real_pay'];
+            }
+            $footer = [['pid' => 'Total: ', 'real_pay' => $totalPay]];
+            trace($list, 'List');
+            $data = array('total'=>$total, 'rows'=>$list, 'footer' => $footer);
             $this->ajaxReturn($data);
         }else{
-            // trace(111);
             $menu_db = D('Menu');
             $currentpos = $menu_db->currentPos(I('get.menuid'));  //栏目位置
             $datagrid = array(
                 'options'       => array(
                     'title'     => $currentpos,
+                    // 'view'    => 'detailview',
+                    // 'detailFormatter' => 'detailFormatter',
+                    // 'onExpandRow' => 'onExpandRow',
                     'url'       => U('Statistics/statisticsList', array('grid'=>'treegrid')),
                     'idField'   => 'id',
                     'treeField' => 'name',
@@ -71,6 +68,62 @@ class StatisticsController extends CommonController {
             $this->assign('datagrid', $datagrid);
             $this->display('statistics_list');
         }
+    }
+    {"total":7,"rows":[
+        {"name":"现金消费(￥)","value":"800","group":"营业情况"},
+        {"name":"会员卡消费(￥)","value":"500","group":"营业情况"},
+        {"name":"会员卡充值(￥)","value":"40","group":"营业情况"},
+        {"name":"新增会员(#)","value":"3","group":"ID Settings"},
+        {"name":"过期会员(#)","value":"3","group":"ID Settings"},
+        {"name":"会员消费人次(#)","value":"123-456-7890","group":"ID Settings"},
+        {"name":"普通消费人次(#)","value":"bill@gmail.com","group":"Marketing Settings"},
+        {"name":"FrequentBuyer","value":"false","group":"Marketing Settings","editor":{
+            "type":"checkbox",
+            "options":{
+                "on":true,
+                "off":false
+            }
+        }}
+    ]}
+
+    public function sublist($pid, $createdtime) {
+        $cost_db = D('Cost');
+        $where = array();
+        foreach ($search as $k=>$v) {
+            if(!$v) continue;
+            switch ($createdtime){
+                case 'day':
+                case 'week':
+                case 'month':
+                case 'year': 
+                    $where = "createdtime" . " between " . self::getBegin($v) . " and " . self::getEnd($v) . " and pid = '" . $pid . "'"; 
+                    break;
+                default:
+                    if ($v[0] != '' && $v[1] == ''){
+                        $where = $k . " >= " . strtotime($v[0]);
+                    } else if($v[0] == '' && $v[1] != ''){
+                        $where = $k . " <= " . strtotime($v[1]);
+                    } else if($v[0] != '' && $v[1] != '') {
+                        $where = $k . " between " . strtotime($v[0]) . " and " . strtotime($v[1]);
+                    }
+                    break;
+            }
+        } 
+        // $where = implode(' and ', $where);
+        trace($where,'Where ');
+        $limit=($page - 1) * $rows . "," . $rows;
+        $total = $cost_db->where($where)->count();
+        $order = "id ".$order;
+        $field= array('pid','real_pay','FROM_UNIXTIME(modified, "%Y-%m-%d %H:%i:%s") as modified','operate_id','point');
+        $list = $total ? $cost_db->field($field)->where($where)->order($order)->limit($limit)->select() : array();
+        $totalPay = 0;
+        foreach ($list as $value) {
+            $totalPay += $value['real_pay'];
+        }
+        $footer = [['pid' => 'Total: ', 'real_pay' => $totalPay]];
+        trace($list, 'List');
+        $data = array('total'=>$total, 'rows'=>$list, 'footer' => $footer);
+        $this->ajaxReturn($data);
     }
 
     #每天的开始和结束
